7c53d7faeca25c56276b4cf0cf672f2497eb5a40
Wang Jing <wangjingnic1993@gmail.com>
2020-07-16 11:11:26 +0800

    String

--- demo/src/main/java/com/example/demo/DemoJava.java
+++ demo/src/main/java/com/example/demo/DemoJava.java
@@ -8,7 +8,6 @@ public class DemoJava {
         double x = 4;
         double y = Math.sqrt(x);// sqrt处理的不是对象，为静态方法
         // System.out.print(y);
-
         double z = Math.pow(x, y);
         // System.out.println(z);
         /**
@@ -17,9 +16,17 @@ public class DemoJava {
          * 而在二进制系统中无法精确地表示分数 1/10。这 就好像十进制无法精确地表示分数 1/3 —样。如果在数值计算中不允许有任何舍入误差， 就应该使用
          * BigDecima丨类
          */
-        System.out.println(2.0 - 1.1);
-
+        // System.out.println(2.0 - 1.1);
+        // \u00A0 is a newline
         // System.out.println(Double.isNaN(z/0));
+        // Java的String是不可修改的，这不同于PHP和C
+        // 不可变字符串却有一个优点：编译器可以让字符串共享。
+        System.out.println("Hello String: " + z);
+        String hello = String.join("*", "H", "E", "L", "L", "O");
+        System.out.println(hello);
+        System.out.println(hello.equals("H*E*L*L*o"));
+        System.out.println(hello.equalsIgnoreCase("H*E*L*L*o"));
+
     }
 
 }
