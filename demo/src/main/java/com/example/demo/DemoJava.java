adde11ca0fc036cc8ae3338a498158fa6240c8de
Wang Jing <wangjingnic1993@gmail.com>
2020-07-16 10:05:38 +0800

    Math

---
+++ demo/src/main/java/com/example/demo/DemoJava.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class DemoJava {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+        //Math
+        double x =4;
+        double y=Math.sqrt(x);
+        System.out.print(y);
+    }
+
+}
