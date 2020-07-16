041670a6b987aed44106191d81e982ce5db32658
Wang Jing <wangjingnic1993@gmail.com>
2020-07-16 15:31:12 +0800

    Scanner

---
+++ demo/src/main/java/com/example/demo/DemoCondition.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+public class DemoCondition {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
--- demo/src/main/java/com/example/demo/DemoScanner.java
+++ demo/src/main/java/com/example/demo/DemoScanner.java
@@ -1,9 +1,23 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 public class DemoScanner {
 
     public static void main(String[] args) {
-
+        // Scanner in = new Scanner(System.in);
+        // // get first input
+        // System.out.print("What is your name? ");
+        // String name = in.nextLine();
+        // // get second input
+        // System.out.print("How old are you? ");
+        // int age = in.nextInt();
+        // // II display output on console
+        // System.out.println("Hello, " + name + (age + 1));
+        String name = "watts";
+        int age = 1;
+        boolean sex = false;
+        System.out.printf("hello, %s fhaha %d ddd %b", name, age,sex);
     }
 
 }
