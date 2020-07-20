881e9a888c3c5a9534b24278e683445919028fee
Wang Jing <wangjingnic1993@gmail.com>
2020-07-20 16:11:44 +0800

    OOP

---
+++ demo/src/main/java/com/example/demo/DemoOOP.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class DemoOOP {
+
+    public static void main(String[] args) {
+
+        // 所有類都源自Object類
+
+        // System.out.println(new Date());
+        // System.out.println(new Date().toString());
+        // 考虑到各地对时间格式（2020-02-03、20200203等）的要求不同，所以用类描述时间
+
+        // Date deadline = new Date();
+        // String s = deadline.toString();
+        // System.out.println(s);
+
+        // System.out.println(LocalDate.now());
+        // System.out.println(LocalDate.of(1994,01,23));
+        LocalDate date = LocalDate.now();
+        // plusDays 方法没有更改原有对象，而是在date的基础上建立了新对象
+        LocalDate aThousDaysLater = date.plusDays(1000);
+        System.out.println(date);
+        System.out.println(aThousDaysLater);
+        // Employee();
+    }
+
+    private String Employee() {
+        String ss = s;
+        System.out.println(ss);
+        return ss;
+    }
+
+}
