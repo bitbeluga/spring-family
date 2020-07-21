f54296b3f8a28847cd01256d55ff69fccd021aeb
Wang Yi <wangjingnic1993@gmail.com>
2020-07-21 21:08:01 +0800

    m

---
+++ demo/src/main/java/com/example/demo/DemoJava.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+public class DemoJava {
+    public static void main(String[] args) {
+        int i = 0;
+    final double DEMO=1;
+    
+        System.out.println("args = " + i + Arrays.deepToString(args));
+        int j = 0;
+    }
+}
